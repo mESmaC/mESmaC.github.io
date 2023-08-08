78d98cd63c4ea41dfe7fa5e9ce20dd7e4307392e
cemerybusiness@gmail.com <cemerybusiness@gmail.com>
2023-08-08 13:01:15 -0400

    Refreshing Repo

--- index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom';
-
-document.body.innerHTML = '<div id="app"></div>';
-
-const root = createRoot(document.getElementById('app'));
-root.render(<html>Hello, React</html>);
